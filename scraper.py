896ac0198966732e9dceadd68eb739b1ed5ab755
gabrielx52 <gabriel.meringolo@gmail.com>
2017-12-12 09:56:35 -0800

    Started it.

--- scraper.py
+++ scraper.py
@@ -1,21 +1,20 @@
+"""Stackoverflow job scraper."""
+from bs4 import BeautifulSoup as BS
+
+import requests
 
 
-Write a real script that really works that'll scrape the first 10 pages of StackOverflow.com/jobs. Here's what information you'll be looking for:
-
-For any job posting that is tagged with Python, save in a row in a CSV file:
-
-- The job title
-- The company name 
-- The location
-- The date posted (in whatever date format makes the most sense to you)
-- The link to the actual job posting
-
-You may save other info as well if you see fit. You must have at least those four. 
-
-The CSV file should be saved in a directory called "results" with the name of the CSV file including today's date and any other information you would find useful in a filename. The file format should be .csv
-
-I should be able to run the script from the command line by typing "python scraper.py". When it's running, it should tell me when it moves to a new page of results, as well as how many job postings it saved when it finishes. Make your messages informative and user friendly. 
-
-You may use whatever Python packages you see fit to do this, though you may want to at least start with "requests" and "beautifulsoup". 
-
-Push your code to GitHub. Gitignore the contents of the "results" directory but NOT the directory itself. Email me the link to your repo by 10am (I'll be in before then)
+def stack_parser():
+    """Parse stackoverflow for python jobs and return CSV."""
+    for i in range(1, 10):
+        url = f'https://stackoverflow.com/jobs?pg={i}'
+        r = requests.get(url)
+        soup = BS(r.content, 'html.parser')
+        page_res = soup.find_all('div', class_='-job')
+        py_tag = soup.find_all(string='python', class_='post-tag') 
+        parent = py_tag.find_parent(class_='-job-summary')
+        title = parent.find('a').text
+        company = parent.find(class_='-name').text.strip()
+        location = parent.find(class_='-location').contents[0].strip()
+        date_posted = parent.find(class_='-posted-date').text.strip()
+        link = parent
